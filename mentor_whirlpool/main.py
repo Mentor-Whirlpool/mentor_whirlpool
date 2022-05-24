0919826db52c499adaae9eb9a267188018250823
zinstack625 <klimanov625@outlook.com>
2022-05-24 14:21:01 +0300

    init database at startup

--- mentor_whirlpool/main.py
+++ mentor_whirlpool/main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 from asyncio import run
-
 from telegram import bot
+from database import Database
+
 # here will be handles importing
 import common
 import generic_handles
 import mentor_handles
 import admin_handles
+
+db = Database()
+db.initdb()
 
 run(bot.polling())
