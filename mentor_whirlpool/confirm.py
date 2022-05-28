83e7675dbba7e53069c2196d8529f43b24f3edfa
zinstack625 <klimanov625@outlook.com>
2022-05-28 23:06:27 +0300

    rework confirm function header

--- mentor_whirlpool/confirm.py
+++ mentor_whirlpool/confirm.py
@@ -4,7 +4,7 @@ from database import Database
 from asyncio import create_task
 
 
-async def confirm(message, callback_yes, callback_no=None):
+async def confirm(message, callback_yes, callback_yes_args=[], callback_no=None, callback_no_args=[]):
     """
     Should ask for user confirmation. If user replies with yes, call the
     callback. Otherwise, if callback_no is not None, call it
