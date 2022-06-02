362d21a12bf7ca9bf63d508b85782eb8bc7db4b6
A-Lazar-A <fub.lazar@mail.ru>
2022-06-03 02:42:31 +0300

    admin_handles: added admin_start, course_works, list_mentors, callback_mentors_info(in progress)

--- mentor_whirlpool/admin_handles.py
+++ mentor_whirlpool/admin_handles.py
@@ -20,7 +20,18 @@ async def admin_start(message):
     iterable
         Iterable with all handles texts
     """
-    raise NotImplementedError
+    return ['Менторы', 'Запросы']
+
+
+@bot.message_handler(func=lambda msg: msg.text == 'Запросы')
+async def course_work(message):
+    db = Database()
+    course_works = await db.get_course_works()
+    messages = []
+    for work in course_works:
+        messages.append(f'{work["student"]}\n{work["description"]}')
+    message_course_works = '--------\n'.join(messages)
+    await bot.send_message(message.chat.id, message_course_works)
 
 
 @bot.message_handler(func=lambda msg: msg.text == 'Добавить ментора')
@@ -53,7 +64,7 @@ async def delete_mentor(message):
     raise NotImplementedError
 
 
-@bot.message_handler(func=lambda msg: msg.text == 'Список менторов')
+@bot.message_handler(func=lambda msg: msg.text == 'Менторы')
 async def list_mentors(message):
     """
     If from_user.id is in admins, print an InlineMarkupKeyboard of mentors.
@@ -64,4 +75,34 @@ async def list_mentors(message):
     message : telebot.types.Message
         A pyTelegramBotAPI Message type class
     """
-    raise NotImplementedError
+    db = Database()
+    mentors = await db.get_mentors()
+    for mentor in mentors:
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton('Выбрать', callback_data='choose_mentor_' + str(mentor['id'])))
+        message_subjects = '\n'.join(mentor['subjects'])
+        await bot.send_message(message.chat.id, f'*@{mentor["name"]}*\n{message_subjects}', reply_markup=markup,
+                               parse_mode='markdown')
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('choose_mentor_'))
+async def callback_mentors_info(call):
+    db = Database()
+    mentors = await db.get_mentors()
+
+    mentor_info = {}
+    for mentor in mentors:
+        if mentor['id'] == int(call.data[14:]):
+            mentor_info = mentor
+            break
+
+    message_subjects = '\n'.join(mentor_info['subjects'])
+    message_students = '\n'.join(str(student) for student in mentor_info['students'])
+    markup = types.InlineKeyboardMarkup()
+    edit_subjects = types.InlineKeyboardButton('Изменить тему', callback_data='edit_subjects')
+    edit_students = types.InlineKeyboardButton('Изменить студентов', callback_data='edit_students')
+    markup.add(edit_students, edit_subjects)
+    await bot.answer_callback_query(call.id)
+    await bot.send_message(call.from_user.id,
+                           f'*@{mentor_info["name"]}*\n{message_subjects}\nStudents\n{message_students}',
+                           reply_markup=markup, parse_mode='markdown')
