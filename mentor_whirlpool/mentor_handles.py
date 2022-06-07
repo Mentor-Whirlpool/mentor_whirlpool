dd6c90bc042d9c13daf40eff3f68b1e03781fb2e
A-Lazar-A <fub.lazar@mail.ru>
2022-06-07 13:38:22 +0300

    mentor_handles: cleaning comments

--- mentor_whirlpool/mentor_handles.py
+++ mentor_whirlpool/mentor_handles.py
@@ -63,18 +63,7 @@ async def works(message):
 async def callback_query_work(call):
     db = Database()
     my_id = (await db.get_mentors(chat_id=call.from_user.id))[0]['id']
-
-    # for mentor in await db.get_mentors():
-    #     if mentor['chat_id'] == call.from_user.id:
-    #         my_id = mentor['id']
-    #         break
-
     course_work_info = (await db.get_course_works(int(call.data[5:])))[0]
-
-    # for work in await db.get_course_works():
-    #     if work['id'] == int(call.data[5:]):
-    #         course_work_description = work['description']
-    #         break
 
     await db.accept_work(my_id, call.data[5:])
     for work in (await db.get_students(student=course_work_info["student"]))[0]['course_works']:
@@ -83,28 +72,6 @@ async def callback_query_work(call):
     await bot.send_message(call.from_user.id,
                            f'Вы взялись за *{course_work_info["description"]}*\nНапишите @{(await db.get_students(student=course_work_info["student"]))[0]["name"]}',
                            )
-
-
-@bot.message_handler(func=lambda msg: msg.text == 'Темы')
-async def subjects(message):
-    """
-    Should send a list of inline buttons from db.get_subjects() and a finish button
-
-    Buttons with subjects can be clicked to narrow the list
-    Clicking on a finish button should produce a list of course works with
-    selected subject. Those buttons should behave the same way they behave in
-    works handle
-
-    Parameters
-    ----------
-    message : telebot.types.Message
-        A pyTelegramBotAPI Message type class
-    """
-    db = Database()
-    subjects_ = await db.get_subjects()
-    markup = types.InlineKeyboardMarkup(row_width=3)
-    markup.add(*[types.InlineKeyboardButton(x, callback_data=x) for x in subjects_])
-    await bot.send_message(message.chat.id, 'Темы', reply_markup=markup)
 
 
 @bot.message_handler(func=lambda msg: msg.text == 'Мои темы')
@@ -124,12 +91,7 @@ async def my_subjects(message):
         A pyTelegramBotAPI Message type class
     """
     db = Database()
-    # my_subjects_ = ['sub1', 'sub2', 'sub3', 'sub4']
     my_subjects_ = (await db.get_mentors(chat_id=message.from_user.id))[0]['subjects']
-    # for mentor in await db.get_mentors():
-    #     if mentor['chat_id'] == message.from_user.id:
-    #         my_subjects_ = mentor['subjects']
-    #         break
 
     markup = types.InlineKeyboardMarkup(row_width=3)
     add = types.InlineKeyboardButton('Добавить', callback_data='sub_to_add')
@@ -166,11 +128,6 @@ async def callback_show_subjects_to_add(call):
     db = Database()
     my_subjects_ = (await db.get_mentors(chat_id=call.from_user.id))[0]['subjects']
 
-    # for mentor in await db.get_mentors():
-    #     if mentor['chat_id'] == call.from_user.id:
-    #         if mentor['subjects'] is not None:
-    #             my_subjects_ = mentor['subjects']
-    #         break
     if not my_subjects_:
         my_subjects_ = []
 
@@ -180,7 +137,6 @@ async def callback_show_subjects_to_add(call):
         if subject in my_subjects_:
             subjects_to_add.remove(subject)
 
-    # subjects_to_add = ['sub1', 'sub2', 'sub3', 'sub4']
     if subjects_to_add:
         markup = types.InlineKeyboardMarkup()
         markup.add(
@@ -194,12 +150,8 @@ async def callback_show_subjects_to_add(call):
 @bot.callback_query_handler(func=lambda call: call.data.startswith('sub_add_'))
 async def callback_add_subject(call):
     db = Database()
-
     my_id = (await db.get_mentors(chat_id=call.from_user.id))[0]['id']
-    # for mentor in await db.get_mentors():
-    #     if mentor['chat_id'] == call.from_user.id:
-    #         my_id = mentor['id']
-    #         break
+
     if not (await db.get_mentors(chat_id=call.from_user.id))[0]['subjects'] or call.data[8:] not in \
             (await db.get_mentors(chat_id=call.from_user.id))[0]['subjects']:
         await db.add_mentor_subjects(my_id, [call.data[8:]])
@@ -214,11 +166,6 @@ async def callback_show_subjects_to_delete(call):
     db = Database()
     my_subjects_ = (await db.get_mentors(chat_id=call.from_user.id))[0]['subjects']
 
-    # for mentor in await db.get_mentors():
-    #     if mentor['chat_id'] == call.from_user.id:
-    #         my_subjects_ = mentor['subjects']
-    #         break
-
     markup = types.InlineKeyboardMarkup()
     markup.add(
         *[types.InlineKeyboardButton(subject, callback_data='sub_delete_' + subject) for subject in my_subjects_])
@@ -231,30 +178,9 @@ async def callback_add_subject(call):
     db = Database()
     my_id = (await db.get_mentors(chat_id=call.from_user.id))[0]['id']
 
-    # for mentor in await db.get_mentors():
-    #     if mentor['chat_id'] == call.from_user.id:
-    #         my_id = mentor['id']
-    #         break
-
     await db.remove_mentor_subject(my_id, call.data[11:])
     await bot.answer_callback_query(call.id)
     await bot.send_message(call.from_user.id, f'Тема *{call.data[11:]}* успешно удалена', parse_mode='markdown')
-
-
-@bot.message_handler(func=lambda msg: msg.text == 'Установить темы')
-async def set_subjects(message):
-    """
-    Should ask for addition/subtraction of subjects. Subsequent messages
-    should be treated accordingly. If in addition mode, an unknown subject is
-    received, add it. If in subtraction mode, an unknown subject is received,
-    reply with a warning and do nothing
-
-    Parameters
-    ----------
-    message : telebot.types.Message
-        A pyTelegramBotAPI Message type class
-    """
-    raise NotImplementedError
 
 
 @bot.message_handler(func=lambda msg: msg.text == 'Мои студенты')
