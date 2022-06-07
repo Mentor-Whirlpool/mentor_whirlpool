2a753b6704b0bee7b57155d936304242b07d4141
A-Lazar-A <fub.lazar@mail.ru>
2022-06-07 16:53:18 +0300

    admin_handles: fix messsage in course_work, cleaning

--- mentor_whirlpool/admin_handles.py
+++ mentor_whirlpool/admin_handles.py
@@ -29,39 +29,9 @@ async def course_work(message):
     course_works = await db.get_course_works()
     messages = []
     for work in course_works:
-        messages.append(f'{work["student"]}\n{work["description"]}')
-    message_course_works = '--------\n'.join(messages)
+        messages.append(f'@{(await db.get_students(work["student"]))[0]["name"]}\n{work["description"]}')
+    message_course_works = '\n--------\n'.join(messages)
     await bot.send_message(message.chat.id, message_course_works)
-
-
-@bot.message_handler(func=lambda msg: msg.text == 'Добавить ментора')
-async def add_mentor(message):
-    """
-    If from_user.id is in admins, parse the message to get new admin chat_id
-    and add it with db.add_admin(). Otherwise, report an error to the sender.
-    Should ask for confirmation
-
-    Parameters
-    ----------
-    message : telebot.types.Message
-        A pyTelegramBotAPI Message type class
-    """
-    raise NotImplementedError
-
-
-@bot.message_handler(func=lambda msg: msg.text == 'Удалить ментора')
-async def delete_mentor(message):
-    """
-    If from_user.id is in admins, parse the message to get admin chat_id
-    and remove it with db.remove_admin(). If the message is empty, remove
-    sender
-
-    Parameters
-    ----------
-    message : telebot.types.Message
-        A pyTelegramBotAPI Message type class
-    """
-    raise NotImplementedError
 
 
 @bot.message_handler(func=lambda msg: msg.text == 'Менторы')
