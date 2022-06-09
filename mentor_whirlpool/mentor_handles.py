64aacf97540926306ce22577c5a34d2b7d11ead3
A-Lazar-A <fub.lazar@mail.ru>
2022-06-09 21:12:39 +0300

    refactor for new version of db, parse_mode -> html

--- mentor_whirlpool/mentor_handles.py
+++ mentor_whirlpool/mentor_handles.py
@@ -53,10 +53,10 @@ async def works(message):
                     f'@{(await db.get_students(work["student"]))[0]["name"]}\n{work["description"]}',
                     callback_data='work_' + str(work['id'])))
 
-        await bot.send_message(message.chat.id, '*Доступные курсовые работы*', reply_markup=markup,
-                               parse_mode='markdown')
+        await bot.send_message(message.chat.id, '<b>Доступные курсовые работы</b>', reply_markup=markup,
+                               parse_mode='html')
     else:
-        await bot.send_message(message.chat.id, '*Сначала добавьте темы*', parse_mode='markdown')
+        await bot.send_message(message.chat.id, '<b>Сначала добавьте темы</b>', parse_mode='html')
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('work_'))
@@ -66,12 +66,10 @@ async def callback_query_work(call):
     course_work_info = (await db.get_course_works(int(call.data[5:])))[0]
 
     await db.accept_work(my_id, call.data[5:])
-    for work in (await db.get_students(student=course_work_info["student"]))[0]['course_works']:
-        await db.remove_course_work(work['id'])
     await bot.answer_callback_query(call.id)
     await bot.send_message(call.from_user.id,
-                           f'Вы взялись за *{course_work_info["description"]}*\nНапишите @{(await db.get_students(student=course_work_info["student"]))[0]["name"]}',
-                           )
+                           f'Вы взялись за <b>{course_work_info["description"]}</b>\nНапишите @{(await db.get_students(id_field=course_work_info["student"]))[0]["name"]}',
+                           parse_mode='html')
 
 
 @bot.message_handler(func=lambda msg: msg.text == 'Мои темы')
@@ -95,17 +93,16 @@ async def my_subjects(message):
 
     markup = types.InlineKeyboardMarkup(row_width=3)
     add = types.InlineKeyboardButton('Добавить', callback_data='sub_to_add')
-    message_subjects = '*Мои темы*\n'
+    message_subjects = '<b>Мои темы</b>\n'
+
     if my_subjects_:
         delete = types.InlineKeyboardButton('Удалить', callback_data='sub_to_delete')
         markup.row(add, delete)
         message_subjects += '\n'.join(my_subjects_)
-
     else:
         markup.add(add)
 
-    await bot.send_message(message.chat.id, f'{message_subjects}', reply_markup=markup,
-                           parse_mode='markdown')
+    await bot.send_message(message.chat.id, f'{message_subjects}', reply_markup=markup, parse_mode='html')
 
 
 # мои темы получаешь еще кнопки с темам, тыкаешь на кнопку получаешь список курсачей по этой теме (тоже кнопками) -> жмешь на курсач и принимаешь его
@@ -120,7 +117,7 @@ async def my_subjects(message):
 #           await db.get_course_works([call.data[8:]])])  # добавление курсачей будет в callback_query_work
 #     await bot.answer_callback_query(call.id)
 #     await bot.send_message(call.from_user.id, f'Курсовые работы по теме *{call.data[8:]}*', reply_markup=markup,
-#                            parse_mode='markdown')
+#                            parse_mode='html')
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'sub_to_add')
@@ -141,9 +138,9 @@ async def callback_show_subjects_to_add(call):
         markup = types.InlineKeyboardMarkup()
         markup.add(
             *[types.InlineKeyboardButton(subject, callback_data='sub_add_' + subject) for subject in subjects_to_add])
-        await bot.send_message(call.from_user.id, '*Все темы*', reply_markup=markup, parse_mode='markdown')
+        await bot.send_message(call.from_user.id, '<b>Все темы</b>', reply_markup=markup, parse_mode='html')
     else:
-        await bot.send_message(call.from_user.id, '*Вы добавили все доступные темы*', parse_mode='markdown')
+        await bot.send_message(call.from_user.id, '<b>Вы добавили все доступные темы</b>', parse_mode='html')
     await bot.answer_callback_query(call.id)
 
 
@@ -155,9 +152,9 @@ async def callback_add_subject(call):
     if not (await db.get_mentors(chat_id=call.from_user.id))[0]['subjects'] or call.data[8:] not in \
             (await db.get_mentors(chat_id=call.from_user.id))[0]['subjects']:
         await db.add_mentor_subjects(my_id, [call.data[8:]])
-        await bot.send_message(call.from_user.id, f'Тема *{call.data[8:]}* успешно добавлена', parse_mode='markdown')
+        await bot.send_message(call.from_user.id, f'Тема <b>{call.data[8:]}</b> успешно добавлена', parse_mode='html')
     else:
-        await bot.send_message(call.from_user.id, f'Тема *{call.data[8:]}* уже была добавлена', parse_mode='markdown')
+        await bot.send_message(call.from_user.id, f'Тема <b>{call.data[8:]}</b> уже была добавлена', parse_mode='html')
     await bot.answer_callback_query(call.id)
 
 
@@ -170,7 +167,7 @@ async def callback_show_subjects_to_delete(call):
     markup.add(
         *[types.InlineKeyboardButton(subject, callback_data='sub_delete_' + subject) for subject in my_subjects_])
     await bot.answer_callback_query(call.id)
-    await bot.send_message(call.from_user.id, '*Удалить тему*', reply_markup=markup, parse_mode='markdown')
+    await bot.send_message(call.from_user.id, '<b>Удалить тему</b>', reply_markup=markup, parse_mode='html')
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('sub_delete_'))
@@ -178,9 +175,9 @@ async def callback_add_subject(call):
     db = Database()
     my_id = (await db.get_mentors(chat_id=call.from_user.id))[0]['id']
 
-    await db.remove_mentor_subject(my_id, call.data[11:])
+    await db.remove_mentor_subjects(my_id, [call.data[11:]])
     await bot.answer_callback_query(call.id)
-    await bot.send_message(call.from_user.id, f'Тема *{call.data[11:]}* успешно удалена', parse_mode='markdown')
+    await bot.send_message(call.from_user.id, f'Тема <b>{call.data[11:]}</b> успешно удалена', parse_mode='html')
 
 
 @bot.message_handler(func=lambda msg: msg.text == 'Мои студенты')
@@ -193,7 +190,6 @@ async def my_students(message):
     if not my_students_:
         str_my_students_ = 'У Вас нет студентов!'
     else:
-
         str_my_students_ = '\n'.join(
             '@' + student['name'] + ' - ' + student["course_works"][0]["description"] for student
             in my_students_)
