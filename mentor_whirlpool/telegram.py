d27776212d16abcc6706a223a6f235510361102b
keyow <kirill500123@gmail.com>
2022-06-10 09:11:18 +0300

    student_handles: Removed redundant flags, added States for each student.
    Also toggled default state memory in telegram bot declaration. Need to add multiple subjects choice (the last thing that is need to be completed)

--- mentor_whirlpool/telegram.py
+++ mentor_whirlpool/telegram.py
@@ -1,4 +1,5 @@
 from telebot.async_telebot import AsyncTeleBot
+from telebot.asyncio_storage import StateMemoryStorage
 from os import environ
 
-bot = AsyncTeleBot(environ['TELEGRAM_BOT_TOKEN'], parse_mode=None)
+bot = AsyncTeleBot(environ['TELEGRAM_BOT_TOKEN'], state_storage=StateMemoryStorage(), parse_mode=None)
