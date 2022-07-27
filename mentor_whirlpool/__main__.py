30f0442540b1fb376da9c8695e0cf95986eb3045
A-Lazar-A <fub.lazar@mail.ru>
2022-07-27 17:58:53 +0300

    added loging config

--- mentor_whirlpool/__main__.py
+++ mentor_whirlpool/__main__.py
@@ -3,6 +3,7 @@
 from asyncio import run
 from mentor_whirlpool.telegram import bot
 from mentor_whirlpool.database import Database
+import logging
 
 # here will be handles importing
 import mentor_whirlpool.common
@@ -13,11 +14,15 @@ import mentor_whirlpool.admin_handles
 import mentor_whirlpool.support_handles
 import mentor_whirlpool.support_request_handler
 
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
+                    datefmt='%H:%M:%S', )
+
 
 async def main():
     db = Database()
     await db.initdb()
     await bot.infinity_polling()
 
-if __name__ == '__main__':
-    run(main())
+
+run(main())
